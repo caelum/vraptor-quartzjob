250547683a67a8ebda741d1918fa8900e2037f2b
Mauricio Aniche <mauricioaniche@gmail.com>
2012-12-07 13:19:30 -0200

    tirando constante daqui pq enche o saco mudar em dois projetos

--- src/main/java/br/com/caelum/vraptor/quartzjob/CronTask.java
+++ src/main/java/br/com/caelum/vraptor/quartzjob/CronTask.java
@@ -2,7 +2,7 @@ package br.com.caelum.vraptor.quartzjob;
 
 public interface CronTask {
 	public static final String EVERY_FIFTEEN_MINUTES = "0 0/15 * * * ?";
-	public static final String EVERY_FIRST_DAY = "0 0 9 1 * * ?";
+	
 	void execute();
 	String frequency();
 }
