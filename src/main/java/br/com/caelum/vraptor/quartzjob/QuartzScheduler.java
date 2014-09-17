111e73b909c59e4b67850383415515817806f21c
Francisco Sokol <chico.sokol@gmail.com>
2014-09-17 19:41:07 -0300

    extracting method

--- src/main/java/br/com/caelum/vraptor/quartzjob/QuartzScheduler.java
+++ src/main/java/br/com/caelum/vraptor/quartzjob/QuartzScheduler.java
@@ -1,13 +1,7 @@
 package br.com.caelum.vraptor.quartzjob;
 
-import static org.quartz.CronScheduleBuilder.cronSchedule;
-import static org.quartz.JobBuilder.newJob;
-import static org.quartz.TriggerBuilder.newTrigger;
-
-import java.util.List;
-
-import javax.inject.Inject;
-
+import br.com.caelum.vraptor.quartzjob.http.HttpRequestExecutor;
+import br.com.caelum.vraptor.quartzjob.http.QuartzHttpRequestJob;
 import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
 import org.quartz.SchedulerException;
@@ -15,8 +9,12 @@ import org.quartz.Trigger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import br.com.caelum.vraptor.quartzjob.http.HttpRequestExecutor;
-import br.com.caelum.vraptor.quartzjob.http.QuartzHttpRequestJob;
+import javax.inject.Inject;
+import java.util.List;
+
+import static org.quartz.CronScheduleBuilder.cronSchedule;
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.TriggerBuilder.newTrigger;
 
 public class QuartzScheduler {
 	public static final String METHOD_FACTORY = "methodFactory";
@@ -41,37 +39,45 @@ public class QuartzScheduler {
 		this.methodFactory = methodFactory;
 	}
 
-	public void configure(List<CronTask> tasks) throws SchedulerException {
-		logger.info("Starting to configure quartz: " + tasks.size() + " tasks found");
+	public void configure(List<CronTask> tasks)  {
+		logger.info("Starting to configure quartz tasks found");
 
-		for(CronTask task : tasks) {
-			linker.linkTo(task).execute();
-			String url = linker.get().replace("https", "http");
-			
-			JobDataMap data = new JobDataMap();
-			data.put("url", url);
-			data.put(METHOD_FACTORY, methodFactory);
-			
-			JobDetail job = newJob(QuartzHttpRequestJob.class)
-					.withIdentity(task.getClass().getName(), JOB_IDENTIFIER)
-					.usingJobData(data)
-					.build();
-
-			Trigger trigger = newTrigger()
-					.withIdentity(task.getClass().getName(), JOB_IDENTIFIER)
-					.withSchedule(cronSchedule(task.frequency()))
-					.forJob(task.getClass().getName(), JOB_IDENTIFIER)
-					.startNow()
-					.build();
-
-			logger.info("Registering " + task.getClass().getName() + " to run every " + task.frequency());
-
-			scheduler.add(job, trigger);
+		try {
+			for(CronTask task : tasks) {
+				configureTrigger(task);
+			}
+			scheduler.start();
+			logger.info("Quartz configured and started!");
+		} catch (Exception e) {
+			logger.error("Error during quartz configuration", e);
 		}
 
-		scheduler.start();
-		logger.info("Quartz configured and started!");
-
-
 	}
+
+	private void configureTrigger(CronTask task) throws SchedulerException {
+
+		linker.<CronTask>linkTo(task).execute();
+		String url = linker.get().replace("https", "http");
+
+		JobDataMap data = new JobDataMap();
+		data.put("url", url);
+		data.put(METHOD_FACTORY, methodFactory);
+
+		JobDetail job = newJob(QuartzHttpRequestJob.class)
+				.withIdentity(task.getClass().getName(), JOB_IDENTIFIER)
+				.usingJobData(data)
+				.build();
+
+		Trigger trigger = newTrigger()
+				.withIdentity(task.getClass().getName(), JOB_IDENTIFIER)
+				.withSchedule(cronSchedule(task.frequency()))
+				.forJob(task.getClass().getName(), JOB_IDENTIFIER)
+				.startNow()
+				.build();
+
+		logger.info("Registering " + task.getClass().getName() + " to run every " + task.frequency());
+
+		scheduler.add(job, trigger);
+	}
+
 }
